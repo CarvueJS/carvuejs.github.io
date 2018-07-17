681e37f1474fd396ffdf75934e98f0a0b9f4dee1
nicholaslee119 <nicholaslee119@gmail.com>
2018-07-17 12:17:10 +0900

    imple the result to navi

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -3,9 +3,14 @@
     <ca-search class="search__input" v-model="searchInput"></ca-search>
     <ca-data-table class="search__result">
     <tbody>
-      <tr v-for='(resultItem, index) in searchResult' :key='index'><th>
-          {{resultItem.name}}
-      </th></tr>
+      <tr v-for='(resultItem, index) in searchResult' :key='index'>
+        <th>
+          <router-link :to="'/component/'+resultItem.name">{{resultItem.name}}</router-link>
+        </th>
+        <th>
+          {{resultItem.content}}
+        </th>
+      </tr>
     </tbody>
   </ca-data-table>
   </div>
@@ -19,7 +24,7 @@ export default {
   name: 'search',
   data: () => ({
     searchInput: '',
-    searchResult: [ 'Nothing matches', ],
+    searchResult: [],
     fuse: null,
     options: {
       matchAllTokens: true,
@@ -44,14 +49,21 @@ export default {
   },
   mounted () {
     // add focus event API to ca-search
-    this.elementInput = document.querySelector('.search__input')
-    this.elementResult = document.querySelector('.search__result')
-    this.elementInput.addEventListener('focus', this.toggleResult, true)
-    this.elementInput.addEventListener('blur', this.toggleResult, true)
+    if (typeof window !== 'undefined') {
+      this.elementInput = document.querySelector('.search__input')
+      this.elementResult = document.querySelector('.search__result')
+      this.elementInput.addEventListener('focus', this.toggleResult, true)
+      this.elementInput.addEventListener('blur', this.toggleResult, true)
+    }
   },
   methods: {
     toggleResult () {
-      this.elementResult.style.visibility = this.elementResult.style.visibility === 'visible' ? 'hidden' : 'visible'
+      // TODO: fix this ugly way
+      if (typeof window !== 'undefined') {
+        setTimeout(() => {
+          this.elementResult.style.visibility = this.elementResult.style.visibility === 'visible' ? 'hidden' : 'visible'
+        }, 1000)
+      }
     },
   },
 }
