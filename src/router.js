4ab05f3f5ad1e9b55a7a1a8d2eef5d8f72daf15d
nicholaslee119 <nicholaslee119@gmail.com>
2018-07-19 16:53:27 +0900

    adjust route

--- src/router.js
+++ src/router.js
@@ -11,7 +11,7 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: Home,
     },
@@ -47,7 +47,7 @@ export default new Router({
     },
     {
       path: '*',
-      redirect: '/component/Accordion',
+      redirect: '/home',
     },
   ],
 })
