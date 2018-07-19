9038d413a9c4bfb268c08123723e0b2209c3e268
nicholaslee119 <nicholaslee119@gmail.com>
2018-07-19 16:49:24 +0900

    adjust build config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production'
-    ? '/carvuejs-website/'
+    ? '/dist/carvuejs-website/'
     : '/',
   chainWebpack: config => {
     config.module
